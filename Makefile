4d70fd22cc88306d55d6d6d936fc7b19cbef32fa
Reza Jelveh <toyb0mb@gmx.net>
2012-01-07 04:53:09 +0100

    add license header

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 all: gtk-nodeco.c
-	gcc -g `pkg-config --cflags --libs gtk+-3.0` gtk-nodeco.c -o gtk-nodeco
+	gcc -g `pkg-config --cflags --libs gtk+-3.0` gtk-nodeco.c -o grfkill
+	strip grfkill
+
+# [~] % for i (*svg) gdk-pixbuf-csource $i --struct --name `echo $i | cut -d '.' -f 1`_inline >| `echo $i | cut -d '.' -f 1`.h
--- gtk-nodeco.c
+++ gtk-nodeco.c
@@ -1,7 +1,31 @@
+/**
+ * Copyright (C) Reza Jelveh <reza.jelveh (at) tuhh.de>
+ * Based on Gary Ching-Pang Lin's gtk example code
+ * https://github.com/lcp/gtk-fun/tree/master/gtk-nodeco
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE. 
+ */
+
 #include <gtk/gtk.h>
 #include <gdk-pixbuf/gdk-pixdata.h>
 #include <stdlib.h>
-
 
 #define BACKGROUND_ALPHA 0.75
 #define ICON_SAPCE 6
